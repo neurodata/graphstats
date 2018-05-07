345beda794b740928a2e591a9af6fe60d6eba1e4
hrbzkm98 <kzhang@jhu.edu>
2018-05-07 04:39:47 -0400

    General functionality testing for dimselect.R

--- tests/testthat/test-ase.R
+++ tests/testthat/test-ase.R
@@ -48,32 +48,54 @@ test_that("Incorrect input graph 'g'.", {
 })
 
 # General Functionality.
-test_that("Correct output for various matrices.", {
+test_that("End-to-end testing.", {
 
-  # Produce random matrix, and embed to 2 dims.
-  n <- 7
-  dim <- 3
-  A <- matrix(rep(0, n*n), nrow = n)
-  for (i in 1:n) {
-    for (j in 1:(i-1)) {
-      A[i, j] <- rbinom(1, 1, 0.5)
-      A[j, i] <- A[i, j]
-    }
+  # Number of simulations. Count how many times latent block assignments are recovered
+  # via kmeans clustering of ASE from a strong block 2-SBM and simple random graph.
+  num_sims <- 10
+  sbm_is_better <- 0
+  er_is_better <- 0
+
+  for (s in 1:num_sims) {
+
+    ## Simulate  core-periphery SBM, and simple ER graph.
+    set.seed(123)
+    n <- 100
+    num_class1 <- n/2
+
+    # SBM Params
+    num_class2 <- n - num_class1
+    assignments <- c(rep(1, num_class1), rep(2, num_class2))
+    B_sbm <- matrix(c(0.8, 0.2,
+                      0.2, 0.8), nrow = 2)
+    p <- 0.5
+    B_er  <- matrix(c(p, p,
+                      p, p), nrow = 2)
+
+    # 2-block simulation.
+    g_sbm <- igraph::sample_sbm(n, pref.matrix=B_sbm, block.sizes=c(num_class1, num_class2))
+    # Simple random graph.
+    g_er <- igraph::sample_sbm(n, pref.matrix=B_er, block.sizes=c(num_class1, num_class2))
+
+    ## Embed both with ASE.
+    dim <- 2
+    X_sbm <- ase(g_sbm, dim)
+    X_er <- ase(g_er, dim)
+
+    ## Perform k-means clustering on embedded data.
+    kmeans_sbm <- kmeans(X_sbm, 2)$cluster
+    kmeans_er <- kmeans(X_er, 2)$cluster
+
+    ## Check ARI of both clustering assignments.
+    ari_sbm <- mclust::adjustedRandIndex(kmeans_sbm, assignments)
+    ari_er <- mclust::adjustedRandIndex(kmeans_er, assignments)
+    if (ari_sbm > ari_er) { sbm_is_better <- sbm_is_better + 1 }
+    else { er_is_better <- er_is_better + 1 }
+
   }
-  #print(A)
 
-  # Compute SVD
-  SVD <- svd(A)
-  U <- SVD$u
-  D <- SVD$d
-  V <- SVD$v
-
-  W_test <- U[,1:dim] %*% diag(sqrt(D[1:dim]))
-  W <- ase(A, dim)
-  #print(D)
-  #print(U_test)
-  #print(W)
-  #print(W - U_test)
+  ## We expect 2-Block SBM to have a higher value.
+  expect_true( sbm_is_better > er_is_better)
 
 })
 
--- tests/testthat/test-dimselect.R
+++ tests/testthat/test-dimselect.R
@@ -45,16 +45,55 @@ test_that("Incorrect input standard deviation vector 'dat'.", {
   expect_error(dimselect(sdev, n), "Input object 'sdev' contains non-numeric values.")
 })
 
+
 # General Functionality.
+test_that("End-to-end testing.", {
 
-test_that("Correct output for various matrices.", {
+  # Run two seperate Wilcoxon tests on a strong 2-block SBM and a simple random graph.
+  num_sims <- 10
+  dim <- 2
+  test_vector <- rep(dim, num_sims)
+  sbm_dimselected <- c()
+  er_dimselected <- c()
 
-  # Define 3 x 3 matrix (triangle graph), and check for correct output.
-  n <- 2
-  A <- c(100, 99, 98, 50, 49, 48, 10, 9, 8)
-  true_elbow = c(3, 6)
-  dim = dimselect(A,n)
-  expect_equal(dim, true_elbow)
+  for (s in 1:num_sims) {
+
+    ## Simulate  core-periphery SBM, and simple ER graph.
+    set.seed(123)
+    n <- 100
+    num_class1 <- n/2
+
+    # SBM Params
+    num_class2 <- n - num_class1
+    assignments <- c(rep(1, num_class1), rep(2, num_class2))
+    B_sbm <- matrix(c(0.8, 0.2,
+                      0.2, 0.8), nrow = 2)
+    p <- 0.5
+    B_er  <- matrix(c(p, p,
+                      p, p), nrow = 2)
+
+    # 2-block simulation.
+    g_sbm <- igraph::sample_sbm(n, pref.matrix=B_sbm, block.sizes=c(num_class1, num_class2))
+    # Simple random graph.
+    g_er <- igraph::sample_sbm(n, pref.matrix=B_er, block.sizes=c(num_class1, num_class2))
+
+    ## Embed both with ASE; get singular values from adjacency matrix;
+    ## select dimenstion with dimselect.
+    A_sbm <- igraph::as_adj(g_sbm)
+    sigma_sbm <- rARPACK::svds(A_sbm,10)$d
+    dim_sbm <- dimselect(sigma_sbm)[1]
+    sbm_dimselected <- append(sbm_dimselected, dim_sbm)
+
+    A_er <- igraph::as_adj(g_er)
+    sigma_er <- rARPACK::svds(A_er,10)$d
+    dim_er <- dimselect(sigma_er)[1]
+    er_dimselected <- append(er_dimselected, dim_er)
+  }
+
+  p_val_sbm = suppressWarnings(wilcox.test(sbm_dimselected, test_vector, correct=FALSE, paired = TRUE)$p.value)
+  p_val_er = suppressWarnings(wilcox.test(er_dimselected, test_vector, correct=FALSE, paired = TRUE)$p.value)
+
+  ## We expect 2-Block SBM to have a higher p-value.
+  expect_true( is.na(p_val_sbm) || p_val_sbm < p_val_er )
 })
-
 
