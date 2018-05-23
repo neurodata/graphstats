d1af3a069b069d3cc6fcb5a0761e028a82cfe5a8
eric bridgeford <ebridge2@jhu.edu>
2018-05-23 07:57:50 -0400

    added data and code

--- R/plot.R
+++ R/plot.R
@@ -1,26 +1,100 @@
 #' Graph plot
 #'
-#' A function that plots a graph.
+#' A function that plots an igraph object, as an adjacency matrix.
 #'
 #' @import ggplot2
 #' @importFrom reshape2 melt
 #' @importFrom plyr mapvalues
-#' @param graph \code{[v, v]} the input graph.
+#' @param g input graph, as an igraph object. See \code{\link[igraph]{graph}} for details.
 #' @param title the title for the square plot. Defaults to \code{""}.
-#' @param xlabel the x label for the square plot. Defaults to \code{""}.
-#' @param ylabel the y label for the square plot. Defaults to \code{""}.
-#' @param legend.name the legend title for the square plot. Defaults to \code{"metric"}.
-#' @param legend.show whether to show the legend on the plot. Defaults to \code{TRUE}.
-#' @param font.size the default font size for the plot text. Axis/legend text is \code{font.size - 2}. Defaults to \code{12}.
-#' @param limits the limits for fill color. Defaults to \code{c(min(graph), max(graph))}.
-#' @param vfactor if TRUE, interpret the vertex labels as a factor. Defaults to \code{FALSE}.
-#' @param ffactor if TRUE, interpret the fill colors as a factor. Defaults to \code{FALSE}.
-#' @param vlist an optional list for the vertex labels ordered according to the adjacency matrix \code{graph}. Defaults to \code{NULL}.
+#' @param xlabel the x label for the square plot. Defaults to \code{"Vertex"}.
+#' @param ylabel the y label for the square plot. Defaults to \code{"Vertex"}.
+#' @param edge.attr the name of the attribute to use for weights. Defaults to \code{FALSE}.
+#' \itemize{
+#' \item{\code{edge.attr==FALSE} plots the graph as a binary adjacency matrix.}
+#' \item{\code{is.character(edge.attr)} plot the graph as a weighted adjacency matrix, with edge-weights for \cdoe{E(g)} given by \code{E(g)[[edge.attr]]}.}
+#' }
+#' @param font.size the default font size for the plot text. Axis/legend text is \code{font.size - 2}. Defaults to \code{NULL}.
+#' \itemize{
+#' \item{\code{is.null(font.size)} uses the default sizing for all fonts.}
+#' \item{\code{!is.null(font.size)} uses \code{font.size} as the font sizing for the plots.}
+#' }
+#' @param vertex.label an attribute for naming the vertices. Defaults to \code{NULL}.
+#' \itemize{
+#' \item{\code{vertex.label==FALSE} name the vertices \code{V(g)} sequentially, as 1, 2, ... n.}
+#' \item{\code{vertex.label==TRUE} name the vertices \code{V(g)} as \code{V(g)$name}.}
+#' }
+#' @param vertex.attr an attribute to color vertices. Defaults to \code{FALSE}.
+#' \itemize{
+#' \item{\code{vertex.attr==FALSE} assumes no grouping of the vertices, and adds no color accordingly.}
+#' \item{\code{is.character(vertex.attr)} assumes a grouping of the vertices given for \code{V(g)} by \code{V(G)[[vertex.attr]]}, and groups the vertices in \code{V(g)} into ordered blocks with color-coding.}
+#' }
+#' @param log.xfm log-transform the edge-weights. Defaults to \code{FALSE}.
+#' \itemize{
+#' \item{\code{log.xfm==FALSE}}
+#' \item{\code{log.xfm == "log"} transform the edge values, using the natural-logarithm operation. See \code{\link[base]{log}} for details. Does not work if there are negative edge-weights.}
+#' \item{\code{log.xfm == "log10"} transform the edge values, using the logarithm-base-10 operation. See \code{\link[base]{log10}} for details. Does not work if there are negative edge-weights.}
+#' \item{\code{! (log.xfm %in% c(FALSE, "log", "log10"))} assumes \code{log.xfm} is a function, and transform the vertices with the provided function.}
+#' }
 #' @return plot the matrix as a plot.
 #' @author Eric Bridgeford
 #' @export
-gs.plot.plot_matrix <- function(mtx, title="",xlabel="", ylabel="", legend.name="metric", legend.show=TRUE,
-                                font.size=12, limits=c(min(mtx), max(mtx)), vfactor=FALSE, ffactor=FALSE, vlist=NULL) {
+gs.plot.plot_adjacency <- function(g, title="",xlabel="Vertex", ylabel="Vertex", edge.attr=FALSE,
+                                font.size=NULL, vertex.label=FALSE, vertex.attr=FALSE, log.xfm=FALSE, eps=0.0001) {
+  # load adjacency matrix as a dense matrix
+  adj <- as_adjacency_matrix(g, edges=edge.attr, names=vertex.label, type="both", sparse=FALSE)
+  adj.data <- melt(adj)  # melt the graph to a data-frame with row and colnames preserved
+  colnames(adj.data) <- c("Source", "Target", "Weight")
+  if (!is.character(edge.attr)) {
+    adj.data$Weight <- factor(adj.data$Weight, levels=c(0, 1), ordered=TRUE)
+    edge.colors <- c("#ffffff", "#000000")  # binarize the graph as white or black according to 0 or 1
+    names(edge.colors) <- levels(adj.data$Weight)  # name the connections 0 or 1 accordingly
+    wt.name <- "Connection"
+  } else {
+    wt.name <- edge.attr
+    if (log.xfm == "log") {
+      log.xfm=log
+    } else if (log.xfm == "log10") {
+      log.xfm = log10
+    }
+    if (log.xfm != FALSE) {
+      # set 0-weight edges to far lower than rest of graph
+      adj.data$Weight[adj.data$Weight == 0] <- min(adj.data$Weight)/100
+    }
+    # color the vertices on a purple scale
+    edge.colors <- c("#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3",  "#4a1486")
+  }
+  #if (is.character(vertex.attr)) {
+    # reorder the vertices so that vertices in same group are together
+
+  #}
+  plot.adj <- ggplot(adj.data, aes(x=Source, y=Target, fill=Weight)) +
+    geom_tile() +
+    xlab(xlabel) +
+    ylab(ylabel) +
+    ggtitle(title) +
+    scale_fill_manual(values=edge.colors, name=wt.name) +
+    theme_bw()
+  if (!is.character(edge.attr)) {
+    plot.adj <- plot.adj +
+      scale_fill_gradientn(colors=edge.colors)
+  }
+  #if (is.character(vertex.attr)) {
+  #  vertices <- colnames(adj)
+  #  jet.colors <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))
+  #  V.gr <- factor(get.vertex.attribute(g, vertex.attr))  # group the vertices by their attribute
+  #  un.vertices <- levels(V.gr)
+  #  attr.colors <- jet.colors(length(un.vertices))
+  #  for (i in 1:length(attr.colors)) {
+  #    vertex.num <- min(which())
+  #    plot.adj <- plot.adj +
+  #      geom_rect()
+  #  }
+  #  plot.adj <- plot.adj +
+  #    geom_tile(data=adj.data, aes(x=Source, y=Target, fill=Fill1), show.legend=TRUE, alpha=0.2) +
+  #    geom_tile(data=adj.data, aes(x=Source, y=Target, fill=Fill2), show.legend=FALSE, alpha=0.2)
+  #}
+}
   dm <- melt(mtx)
   colnames(dm) <- c("x", "y", "value")
   if (vfactor) {
--- R/sbm.R
+++ R/sbm.R
@@ -3,8 +3,7 @@
 #' A function to fit a stochastic block model to a weighted or unweighted graph.
 #'
 #' @param g an \code{igraph} object. See \code{\link[igraph]{graph}} for details.
-#' @param communities a \code{[[n]]} list indicating the community assignments of vertices in \code{graph}. That is, all vertices in \code{communities} are in \code{V(graph)},
-#' all vertices in \code{V(graph)} are assigned a community, and no vertex in \code{V(graph)} is assigned to multiple communities.
+#' @param community.attribute the attribute of the graph vertices denoting the vertex communities. Should be that \code{community.attr %in% names(vertex.attributes(g))}.
 #' @return the \code{[n, n]} probability matrix for each block, as an \code{igraph} object.
 #' Vertices will be the \code{names(communities)}. Each vertex in the resulting graph will have an attribute \code{community} indicating the vertices in \code{graph} that
 #' comprise the particular community the vertex summarizes See \code{\link[igraph]{graph}} for details.
@@ -17,39 +16,30 @@
 #' sbm.g <- gs.sbm.fit(g, communities)  # fit SBM to graph
 #' @author Eric Bridgeford
 #' @export
-gs.sbm.fit <- function(g, communities) {
-  V.communities <- do.call(c, communities)  # get the union of vertices indicated in communities
-
-  # basic checks
-  # all vertices in communities are in V(graph)
-  if (!all(V.communities %in% V(g))) {
-    stop("communities is not a disjoint subset of V(graph). Vertices in communities exist that do not exist in V(graph).")
+gs.sbm.fit <- function(g, community.attribute) {
+  # get vertex names. If failure, just use 1:length(V(g))
+  V.names <- get.vertex.attribute(g, name = "name")
+  if (is.null(V.names)) {
+    V.names <- 1:length(V(g))  # vertices are the numeric values of the vertices
   }
-  # all vertices in V(graph) are assigned a community
-  if (any(!(V(g) %in% V.communities))) {
-    stop("communities is not a disjoint subset of V(graph). Vertices in V(graph) are not assigned to a community.")
-  }
-  if (length(unique(V.communities)) != length(V.communities)) {
-    stop("communities is not a disjoint subset of V(graph). There exists a vertex(ices) assigned to multiple communities.")
-  }
-
   # meat and taters of the algorithm
   g.mtx <- as_adjacency_matrix(g)  # convert to sparse adjacency matrix for simplicity
-  V.P <- names(communities)  # the vertex names for P are the names of the communities
+  V.comm <- get.vertex.attribute(g, community.attribute)
+  un.comm <- unique(V.c)
   P.mtx <- matrix(NaN, nrow=length(V.P), ncol=length(V.P))  # pre-allocate P for speed; P is dense so just use a regular matrix
   colnames(P.mtx) <- V.P  # assign the vertex names for the new P matrix
   rownames(P.mtx) <- V.P
-  for (i in 1:length(communities)) {
-    for (j in 1:length(communities)) {
-      sg.ij <- g.mtx[communities[[V.P[i]]], communities[[V.P[j]]]]
+  for (i in 1:length(un.comm)) {
+    for (j in 1:length(un.comm)) {
+      sg.ij <- g.mtx[V.comm == un.comm[i], un.comm[j]]
       P.mtx[i, j] <- sum(sg.ij)/length(sg.ij)  # compute the average value within the subgraph i, j
     }
   }
 
   sbm.model <- graph_from_adjacency_matrix(P.mtx, weighted=TRUE)  # create a new matrix for P parameter of SBM
   # assign community attribute to each vertex indicating which vertices in the original graph comprise the new vertex in the SBM
-  for (V.Pi in V.P) {
-    sbm.model <- set_vertex_attr(sbm.model, "community", index=V.Pi, list(vertices=communities[[V.Pi]]))
+  for (commi in un.comm) {
+    sbm.model <- set_vertex_attr(sbm.model, "community", index=V.Pi, list(vertices=V.names[which(V.comm == commi)]))
   }
   return(sbm.model)
 }
