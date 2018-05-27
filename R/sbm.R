8146df5b972e618d20ba0e8918ac5c5e2b0b08e7
Eric Bridgeford <ebridge2@jhu.edu>
2018-05-27 14:58:14 -0400

    updated connectome coding

--- R/sbm.R
+++ R/sbm.R
@@ -4,20 +4,30 @@
 #'
 #' @param g an \code{igraph} object. See \code{\link[igraph]{graph}} for details.
 #' @param community.attribute the attribute of the graph vertices denoting the vertex communities. Should be that \code{community.attribute %in% names(vertex.attributes(g))}.
+#' @param edge.attr the name of the attribute to use for weights. Defaults to \code{NULL}.
+#' \itemize{
+#' \item{\code{is.null(edge.attr)} plots the graph as a binary adjacency matrix.}
+#' \item{\code{is.character(edge.attr)} plot the graph as a weighted adjacency matrix, with edge-weights for \code{E(g)} given by \code{E(g)[[edge.attr]]}.}
+#' }
+#' @param vertex.label an attribute for naming the vertices. Defaults to \code{NULL}.
+#' \itemize{
+#' \item{\code{vertex.label==FALSE} name the vertices \code{V(g)} sequentially, as 1, 2, ... n.}
+#' \item{\code{vertex.label==TRUE} name the vertices \code{V(g)} as \code{V(g)$name}.}
+#' }
 #' @return the probability matrix for each block, as an \code{igraph} object.
 #' Vertices will be the unique communities for attribute \code{community.attribute}. Each vertex in the resulting graph will have an attribute \code{community} indicating the vertices in \code{graph} that
 #' comprise the particular community the vertex summarizes See \code{\link[igraph]{graph}} for details.
 #'
 #' @author Eric Bridgeford
 #' @export
-gs.sbm.fit <- function(g, community.attribute) {
+gs.sbm.fit <- function(g, community.attribute, edge.attr=NULL, vertex.label=FALSE) {
   # get vertex names. If failure, just use 1:length(V(g))
   V.names <- get.vertex.attribute(g, name = "name")
   if (is.null(V.names)) {
     V.names <- 1:length(V(g))  # vertices are the numeric values of the vertices
   }
   # meat and taters of the algorithm
-  g.mtx <- as_adjacency_matrix(g)  # convert to sparse adjacency matrix for simplicity
+  g.mtx <- as_adjacency_matrix(g, attr=edge.attr, names=vertex.label, type="both", sparse=FALSE)  # convert to sparse adjacency matrix for simplicity
   V.comm <- get.vertex.attribute(g, community.attribute)
   un.comm <- unique(V.comm)
   P.mtx <- matrix(NaN, nrow=length(un.comm), ncol=length(un.comm))  # pre-allocate P for speed; P is dense so just use a regular matrix
